3c1b5eb6253918b6e6d44e02bbacb5d1a88fa0ae
kamscode <kamnoah88@gmail.com>
2023-04-10 19:57:35 +0300

    dataTypes and operators added

---
+++ dataTypes.js
@@ -0,0 +1,23 @@
+//primitive Data Types...
+
+//Numbers
+const day = 5;
+console.log(typeof day);
+
+//Strings
+const name = "Dev Ed";
+console.log(typeof name);
+
+//Booleans
+const isLogged = true;
+
+//Undefined
+let day1;
+console.log(day1);
+//Null
+let minute = null;
+
+//symbol
+const name1 = Symbol;
+
+//OBJECT
---
+++ operators.js
@@ -0,0 +1,8 @@
+let inbox = 10;
+
+console.log(inbox / 2);
+console.log(inbox * 2);
+console.log(inbox ** 2);
+console.log(inbox % 2);
+
+//practicing with operators
