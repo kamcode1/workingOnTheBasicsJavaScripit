1e69a702beb3208b9724c70d9af7e98fe45a9ece
kamscode <kamnoah88@gmail.com>
2023-04-10 19:45:06 +0300

    variables added

---
+++ variables.js
@@ -0,0 +1,20 @@
+//creating a let variable
+let inbox1 = 100;
+
+ alert(inbox1);
+
+ inbox1 = 250;
+console.log(inbox1);
+//const value cannot be reassigned inbox
+//const inbox =100;
+
+//console.log(inbox);
+
+let user = 'Ed';
+
+let yourVideos = 20;
+let yourEmail = 30;
+
+let yourVideos1= 20;
+let your_videos=21;
+let _cat = 'meow';
