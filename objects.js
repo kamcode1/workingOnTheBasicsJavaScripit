792a2d57cb142281c023d8749ff8a3ccfe829f23
kamscode <kamnoah88@gmail.com>
2023-04-11 21:01:46 +0300

    objects added

---
+++ objects.js
@@ -0,0 +1,12 @@
+const user = {
+    name: "Ed",
+    age: 25,
+    married: false,
+    greet: function () {
+      console.log("Hello there!");
+    },
+  };
+  
+  console.log(user.name);
+  //object creation
+  
